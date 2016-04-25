03cde7117d692f4bde9c7265d2d8a87241b567b8
Maxime Chamot <maxime.chamot@epitech.eu>
2016-04-25 21:37:43 +0100

    Fixing bug

--- stack.h
+++ stack.h
@@ -1,28 +1,28 @@
 #ifndef _STACK_H_
 # define _STACK_H_
 
-typedef struct		Node
+typedef struct	Node
 {
-	void			*data;
-	struct Node		*prev;
-}					Node;
+  void		*data;
+  struct Node	*prev;
+}		Node;
 
-typedef struct		Stack
+typedef struct	Stack
 {
-	// Properties
-	unsigned int	len;
-	struct Node		*head;
+  // Properties
+  unsigned int	len;
+  struct Node	*head;
 
-	// Member functions
-	unsigned int	(*push)(struct Stack *th, void *data);
-	void *			(*pop)(struct Stack *th);
-	void			(*clear)(struct Stack *th);
-	unsigned int	(*size)(struct Stack *th);
-	void			(*view)(struct Stack *, void(*display)(void *));
-}					Stack;
+  // Member functions
+  unsigned int	(*push)(struct Stack *th, void *data);
+  void *	(*pop)(struct Stack *th);
+  void		(*clear)(struct Stack *th);
+  unsigned int	(*size)(struct Stack *th);
+  void		(*view)(struct Stack *, void(*display)(void *data));
+}		Stack;
 
-void				StackInit(struct Stack *th);
-struct Stack *		NewStack(void);
-void				StackDestroy(struct Stack *th);
+void		StackInit(struct Stack *th);
+struct Stack *	NewStack(void);
+void		StackDestroy(struct Stack *th);
 
 #endif // _STACK_H_
