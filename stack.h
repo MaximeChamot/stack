201fd122c9904c7b66fdfb8079b0239fee52c1ca
Maxime Chamot <maxime.chamot@epitech.eu>
2016-04-25 21:01:52 +0100

    adding files

---
+++ stack.h
@@ -0,0 +1,28 @@
+#ifndef _STACK_H_
+# define _STACK_H_
+
+typedef struct		Node
+{
+	void			*data;
+	struct Node		*prev;
+}					Node;
+
+typedef struct		Stack
+{
+	// Properties
+	unsigned int	len;
+	struct Node		*head;
+
+	// Member functions
+	unsigned int	(*push)(struct Stack *th, void *data);
+	void *			(*pop)(struct Stack *th);
+	void			(*clear)(struct Stack *th);
+	unsigned int	(*size)(struct Stack *th);
+	void			(*view)(struct Stack *, void(*display)(void *));
+}					Stack;
+
+void				StackInit(struct Stack *th);
+struct Stack *		NewStack(void);
+void				StackDestroy(struct Stack *th);
+
+#endif // _STACK_H_
---
+++ test.cpp
@@ -0,0 +1,78 @@
+#include "stdafx.h"
+#include "stack.h"
+
+void display(void *data)
+{
+	int *nbr = (int *)(data);
+	printf("data: %d\n", *nbr);
+}
+
+int main()
+{
+	int tab[] = { 10, 15, 20, 25, 30, 1, 2 };
+	Stack		*newStack;
+
+	newStack = NewStack();
+
+	newStack->push(newStack, &tab[0]);
+	newStack->push(newStack, &tab[1]);
+	newStack->push(newStack, &tab[2]);
+	newStack->push(newStack, &tab[3]);
+	newStack->push(newStack, &tab[4]);
+
+	newStack->view(newStack, &display);
+	printf("---------- Size: %ld\n", newStack->size(newStack));
+
+	newStack->push(newStack, &tab[5]);
+	newStack->push(newStack, &tab[6]);
+
+	newStack->view(newStack, &display);
+	printf("---------- Size: %ld\n", newStack->size(newStack));
+
+	newStack->pop(newStack);
+	newStack->pop(newStack);
+	newStack->pop(newStack);
+	newStack->pop(newStack);
+	newStack->pop(newStack);
+	newStack->pop(newStack);
+	newStack->pop(newStack);
+	newStack->pop(newStack);
+	newStack->pop(newStack);
+	newStack->pop(newStack);
+	newStack->pop(newStack);
+	newStack->pop(newStack);
+	newStack->pop(newStack);
+	newStack->pop(newStack);
+
+	newStack->view(newStack, &display);
+	printf("---------- Size: %ld\n", newStack->size(newStack));
+
+	StackDestroy(newStack);
+
+	newStack->view(newStack, &display);
+	printf("---------- Size: %ld\n", newStack->size(newStack));
+
+	newStack->push(newStack, &tab[0]);
+	newStack->push(newStack, &tab[1]);
+	newStack->push(newStack, &tab[2]);
+	newStack->push(newStack, &tab[3]);
+	newStack->push(newStack, &tab[4]);
+
+	newStack->view(newStack, &display);
+	printf("---------- Size: %ld\n", newStack->size(newStack));
+
+	newStack->clear(newStack);
+	newStack->clear(newStack);
+	newStack->clear(newStack);
+	newStack->clear(newStack);
+	newStack->clear(newStack);
+
+	newStack->view(newStack, &display);
+	printf("---------- Size: %ld\n", newStack->size(newStack));
+
+#ifdef _WIN32
+	system("PAUSE");  /* Pour la console Windows. */
+#endif
+
+	return 0;
+}
